be82650547e381b2dba34805eba76f6199bdd413
maksud190 <maknep3rdoct@gmail.com>
2022-09-15 13:48:29 +0600

    card responsive done

--- history.js
+++ history.js
@@ -3,6 +3,7 @@ const histories = document.getElementById("histories");
 function addHistory(questionText, timeTaken, errorCount) {
   const newRow = document.createElement("div");
   newRow.classList.add("card");
+  
 
   newRow.innerHTML = `
   <h3>${questionText}</h3>
@@ -28,12 +29,14 @@ function displayHistory() {
   previousTests.forEach((test) => {
     const newRow = document.createElement("div");
     newRow.classList.add("card");
-
+    
+    
     newRow.innerHTML = `
-  <h3>${test.questionText}</h3>
-  <p>You took: <span class="bold">${test.timeTaken}</span> seconds</p>
+    <h3>${test.questionText}</h3>
+    <p>You took: <span class="bold">${test.timeTaken}</span> seconds</p>
     <p>You made <span class="bold red">${test.errorCount}</span> mistakes</p>
-  `;
+    `;
+    // histories.innerText= '';
 
     histories.appendChild(newRow);
   });
